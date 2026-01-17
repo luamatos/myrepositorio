9f85f12820cca84a97776d5080d72dda8cab0cab
Lua <131722047+luamatos@users.noreply.github.com>
2026-01-17 13:46:08 +0000

    salvar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
-st.title("TechScool My new app")
-st.header ("First", divider=True)
+st.title("TechScool My new app") #titulo
+st.header ("First", divider=True) #header
 st.subheader("apaixonadas por tecnologia")
 st.write(
     "Primeiro tutorial")
+#markdonw
+st.markdown("# teste")
+st.markdown("preste **atençao**, *impor*")
+st.markdown("""---""")
+st.text("Lista")
+st.markdown("""
+            1. Criar
+            2. anotar
+            3. concluido
+            """)
